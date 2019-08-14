46277d116c272a10b1d33c8b8e8435c2b1bcc908
Kalyan Aravapalli <31206412+kcaravapalli@users.noreply.github.com>
2019-08-14 20:04:58 +0530

    Updated the model path value

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/6v4r44c67de1y2s/blindness-detection.pkl?dl=1'
+export_file_name = 'blindness-detection.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = [0,1,2,3,4]
 path = Path(__file__).parent
 
 app = Starlette()
